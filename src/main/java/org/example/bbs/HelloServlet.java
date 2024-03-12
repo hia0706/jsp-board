9c73cf81e591d046fcbf3924e9e6037416f2aa49
hia <ljjs1215@gmail.com>
2024-03-12 18:15:55 +0900

    Init

---
+++ src/main/java/org/example/bbs/HelloServlet.java
@@ -0,0 +1,10 @@
+package org.example.bbs;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+}
