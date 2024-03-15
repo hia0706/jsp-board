9fe83c2075884b0b9c1487c24a31defa6dc80732
hia <ljjs1215@gmail.com>
2024-03-15 15:59:22 +0900

    Feat 게시글 삭제 구현 및 삭제 시 confirm 추가

--- src/main/java/org/example/bbs/bbs/BbsDAO.java
+++ src/main/java/org/example/bbs/bbs/BbsDAO.java
@@ -142,4 +142,16 @@ public class BbsDAO {
         }
         return -1; // 데이터베이스 오류
     }
+
+    public int delete(int bbsID) {
+        String sql = "update bbs set bbsAvailable = 0 where bbsID = ?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, bbsID);
+            return pstmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1; // 데이터베이스 오류
+    }
 }
